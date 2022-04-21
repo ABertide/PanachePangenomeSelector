706a8eab065f6d0589ac22a22526e983b6fa83b0
Bertide Allan (ext) FRSS <allan.bertide@syngenta.com>
2022-04-21 15:03:34 +0200

    config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? '/PanachePangenomeSelector/' : '/'
+});
