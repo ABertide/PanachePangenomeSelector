d3d7e99aa22247d135657e3716b4450dceedaac3
Bertide Allan (ext) FRSS <allan.bertide@syngenta.com>
2022-05-23 16:23:27 +0200

    debug pangenome panel and checkboxes

--- src/components/PangenomePanel.vue
+++ src/components/PangenomePanel.vue
@@ -1,26 +1,26 @@
 <template>
     <div v-if="pangenomesToTable">
         <h2 class="mb-4">Pangenome Panel</h2>
+
+        !-- file upload -->
         <v-file-input class="mb-5" v-model="pangenomeFile" accept=".csv, .tsv" label="Choose a file or drop it here..." show-size counter @change="onChangeFile"></v-file-input>
+
         <div class="mb-5">
             <h4>Chose Pangenomes</h4>
+
+            <!-- Filter -->
             <v-text-field v-model="pangenomeSearched" label="Enter a pangenome id"></v-text-field>
             <!-- <b-dropdown-divider></b-dropdown-divider> -->
         </div>
         <div>
             <h4>List of Pangenome</h4>
 
-            <va-data-table
-                :items="pangenomesToTable"
-                :columns="headers"
-                :selectable="true"
-                v-model="selectedItems"
-                select-mode="single"
-                @mouseover="myRowHover($event)"
-                @mouseout="myUnRowHover($event)"
-                @selection-change="selectAction"
-                @click="updateSelectPangenomes"
-            >
+            <!-- Table -->
+            <va-data-table :items="pangenomesToTable" :columns="headers" @mouseover="myRowHover($event)" @mouseout="myUnRowHover($event)">
+                <!-- Content -->
+                <template #cell(selected)="data">
+                    <va-checkbox v-model="selectedItems" :array-value="data.cells[3].value" @click="selectAction(data.cells[3].value)" />
+                </template>
                 <template #cell(icon)="data">
                     <img style="height: 40px" :src="data.value" />
                 </template>
@@ -34,6 +34,8 @@
                     <span>{{ data.value }}</span>
                 </template>
             </va-data-table>
+
+            <!-- Submit -->
             <v-btn v-if="!submited" :color="isSubmitable ? 'secondary' : 'grey'" :disabled="isSubmitable ? false : true" @click="submit"> Submit </v-btn>
             <v-btn v-else disabled> Submited </v-btn>
         </div>
@@ -42,16 +44,24 @@
 
 <script>
     import { ref } from '@vue/reactivity';
-    import { computed } from '@vue/runtime-core';
+    import { computed, watch } from '@vue/runtime-core';
     import { useStore } from 'vuex';
     export default {
         setup() {
             const store = useStore();
+
+            // var : Filtering
             const pangenomeSearched = ref('');
+
+            // var : Upload external file
             const pangenomeFile = ref([]);
             let fileinput = ref('');
-            let submited = ref(false);
+
+            // Table selected items
             let selectedItems = ref([]);
+
+            // Submission
+            let submited = ref(false);
             const isSubmitable = computed(() => {
                 if (selectedItems.value.length > 0) {
                     return true;
@@ -59,70 +69,22 @@
                     return false;
                 }
             });
+
+            // Assemblies
             const assemblies = computed(() => {
                 return store.state.assemblies.assemblies;
             });
             const assembliesSelected = computed(() => store.getters['assemblies/assembliesSelected']);
-            // const selectable =
-            const pangenomes = computed(() => store.state.pangenomes.pangenomes);
-            let pangenomesToTable = computed(() => {
-                let pangenomeList = pangenomes.value.map(pangenome => {
-                    let percentage = 0;
-                    if (assembliesSelected.value.length !== 0) {
-                        const assemblyIntersect = assembliesSelected.value.filter(assembly => pangenome.assemblies.includes(assembly.assembly_name));
-                        percentage = (assemblyIntersect.length / pangenome.assemblies.length) * 100;
-                    }
-                    const tableItem = {
-                        percentage: Math.round(percentage),
-                        id: pangenome.id,
-                        panID: pangenome.name,
-                        date: pangenome.creationDate
-                    };
-                    if (percentage === 0) {
-                        tableItem['icon'] = require('@/assets/0.png');
-                    } else if (percentage <= 25) {
-                        tableItem['icon'] = require('@/assets/25.png');
-                    } else if (percentage <= 50) {
-                        tableItem['icon'] = require('@/assets/50.png');
-                    } else if (percentage <= 75) {
-                        tableItem['icon'] = require('@/assets/75.png');
-                    } else {
-                        tableItem['icon'] = require('@/assets/100.png');
-                    }
-                    return tableItem;
-                });
-                if (pangenomeSearched.value != null && pangenomeList != undefined) {
-                    pangenomeList = pangenomeList.filter(pangenome => pangenome.panID.toLowerCase().indexOf(pangenomeSearched.value.trim().toLowerCase()) > -1);
-                }
-                return pangenomeList;
-            });
 
-            const pangenomesID = ref(pangenomesToTable.value.map(pangenome => pangenome.panID));
-            // watch(selectedItems)
-            const headers = computed(() => {
-                const header = [
-                    { key: 'icon', label: 'Assemblies selected', sortable: true },
-                    { key: 'id', label: 'Id', sortable: true },
-                    { key: 'panID', label: 'PanID', sortable: true },
-                    { key: 'date', label: 'Creation Date', sortable: true }
-                ];
-                return header;
-            });
-            const submit = () => {
-                submited.value = true;
-            };
+            // Load pangenomes
+            const pangenomes = computed(() => store.state.pangenomes.pangenomes);
+
+            // File uploading
 
             const onChangeFile = e => {
                 let files = e.target.files || e.dataTransfer.files;
                 if (!files.length) return;
                 createInput(files[0]);
-            };
-
-            const updateSelectPangenomes = () => {
-                const currentSelectedPangenome = selectedItems.value[0];
-                if (currentSelectedPangenome !== undefined) {
-                    store.dispatch('pangenomes/updateIsSelectedStateAction', currentSelectedPangenome);
-                }
             };
 
             const createInput = file => {
@@ -163,6 +125,99 @@
                     }
                 );
             };
+
+            // Table
+
+            // Headers
+
+            const headers = computed(() => {
+                const header = [
+                    { key: 'selected', label: '', sortable: false },
+                    { key: 'icon', label: 'Assemblies selected', sortable: true },
+                    { key: 'id', label: 'Id', sortable: true },
+                    { key: 'panID', label: 'PanID', sortable: true },
+                    { key: 'date', label: 'Creation Date', sortable: true }
+                ];
+                return header;
+            });
+
+            // Content
+            // Rewrite pangenomes object with information for the table
+            let pangenomesToTable = computed(() => {
+                let pangenomeList = pangenomes.value.map(pangenome => {
+                    let percentage = 0;
+                    if (assembliesSelected.value.length !== 0) {
+                        // Percentage
+                        const assemblyIntersect = assembliesSelected.value.filter(assembly => pangenome.assemblies.includes(assembly.assembly_name));
+                        percentage = (assemblyIntersect.length / assembliesSelected.value.length) * 100;
+                    }
+
+                    // Main information to display
+                    const tableItem = {
+                        percentage: Math.round(percentage),
+                        id: pangenome.id,
+                        panID: pangenome.name,
+                        selected: pangenome.selected,
+                        date: pangenome.creationDate
+                    };
+                    // Percentage icon
+                    if (percentage === 0) {
+                        tableItem['icon'] = require('@/assets/0.png');
+                    } else if (percentage <= 25) {
+                        tableItem['icon'] = require('@/assets/25.png');
+                    } else if (percentage <= 50) {
+                        tableItem['icon'] = require('@/assets/50.png');
+                    } else if (percentage <= 75) {
+                        tableItem['icon'] = require('@/assets/75.png');
+                    } else {
+                        tableItem['icon'] = require('@/assets/100.png');
+                    }
+                    return tableItem;
+                });
+                // Hide pangenomes in functions of filter below
+                if (pangenomeSearched.value != null && pangenomeList != undefined) {
+                    pangenomeList = pangenomeList.filter(pangenome => pangenome.panID.toLowerCase().indexOf(pangenomeSearched.value.trim().toLowerCase()) > -1);
+                }
+                return pangenomeList;
+            });
+
+            // const pangenomesID = ref(pangenomesToTable.value.map(pangenome => pangenome.panID));
+
+            // Selection handeling
+            const pangenomesSelectedStored = computed(() => {
+                return store.getters['pangenomes/pangenomesSelected'];
+            });
+
+            // Each time store change
+            watch(pangenomesSelectedStored, newPangenome => {
+                // Reset the checkbox in fonction of pangenome stored
+                if (newPangenome[0] !== undefined) {
+                    selectedItems.value = [newPangenome[0].name];
+                } else {
+                    selectedItems.value = [];
+                }
+            });
+
+            // on Checkbox event
+            const selectAction = panID => {
+                // Set true or false to the selected pangenome or not in the store
+
+                // Get pangenome table object
+                let currentClickedPangenome = pangenomesToTable.value.filter(pangenome => pangenome.panID === panID);
+
+                if (pangenomesSelectedStored.value.length === 0) {
+                    store.dispatch('pangenomes/updateIsSelectedStateAction', currentClickedPangenome[0]);
+                } else {
+                    if (pangenomesSelectedStored.value[0].name !== currentClickedPangenome[0].panID) {
+                        store.dispatch('pangenomes/updateIsSelectedStateAction', currentClickedPangenome[0]);
+                    } else {
+                        store.dispatch('pangenomes/updateIsNotSelectedStateAction', currentClickedPangenome[0]);
+                    }
+                }
+            };
+
+            // Interactions with Graph
+            // On Hover
             const myRowHover = e => {
                 let outerText = '';
                 if (e.target.__vnode.type === 'td') {
@@ -172,7 +227,6 @@
                     // Checkbox case
 
                     outerText = e.target.parentElement.parentElement.parentElement.outerText;
-                    console.log(outerText);
                 } else if (e.target.className === 'va-checkbox__square') {
                     // Checkbox case
 
@@ -190,11 +244,11 @@
                     if (pangenomeSelected.length === 0) {
                         store.state.chart.chart.instance
                             .series()
-                            .points(p => assemblies_names.includes(p.name) && p.selected === true)
+                            .points(p => !assemblies_names.includes(p.name) && p.selected === true)
                             .options({ color: '#c95e00' });
                         store.state.chart.chart.instance
                             .series()
-                            .points(p => assemblies_names.includes(p.name) && p.selected === false)
+                            .points(p => !assemblies_names.includes(p.name) && p.selected === false)
                             .options({ color: 'gray' });
                     } else {
                         const assembliesSelected = pangenomeSelected.map(pangenome => pangenome.assemblies).flat();
@@ -221,16 +275,8 @@
                     }
                 }
             };
-            const selectAction = SelectionChangeEmit => {
-                const currentPangenome = SelectionChangeEmit.currentSelectedItems[0];
-                const previousPangenome = SelectionChangeEmit.previousSelectedItems[0];
-                if (previousPangenome !== undefined && currentPangenome !== undefined) {
-                    if (previousPangenome.panID === currentPangenome.panID) {
-                        store.dispatch('pangenomes/updateIsNotSelectedStateAction', currentPangenome);
-                        selectedItems.value = [];
-                    }
-                }
-            };
+
+            // On unHover
             const myUnRowHover = () => {
                 const pangenomeSelected = pangenomes.value.filter(pangenome => pangenome.selected);
                 let pangenomeAssembliesSelected = pangenomeSelected.map(pangenome => pangenome.assemblies).flat();
@@ -255,6 +301,12 @@
                 }
             };
 
+            // button Submit Action
+            const submit = () => {
+                submited.value = true;
+                console.log(pangenomesSelectedStored.value);
+            };
+
             return {
                 pangenomeSearched,
                 pangenomeFile,
@@ -265,13 +317,14 @@
                 pangenomesToTable,
                 submited,
                 submit,
-                pangenomesID,
+                // pangenomesID,
                 onChangeFile,
                 myUnRowHover,
                 myRowHover,
                 assembliesSelected,
                 selectAction,
-                updateSelectPangenomes
+                // checkSelected,
+                pangenomesSelectedStored
             };
         }
     };
